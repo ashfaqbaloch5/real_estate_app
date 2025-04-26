a7b6d41391f3a200a5e14d48d9e0541915751936
ashfaq_baloch5 <accoun7867@gmail.com>
2025-04-26 20:05:53 +0500

    Add some code

--- app/(roots)/(tabs)/index.tsx
+++ app/(roots)/(tabs)/index.tsx
@@ -5,13 +5,6 @@ export default function Index() {
   return (
     <View className="flex-1 items-center justify-center ">
       <Text className=" font-bold text-lg my-10 text-red-500 font-rubik text-3xl">Welcome to the app!</Text>
-      <View className="text-white">
-      <Link href="/sign_in">Sign in</Link>
-      <Link href="/explore">explore</Link>
-      <Link href="/profile">profile</Link>
-      <Link href="/properties/1">Property</Link>
-      </View>
-      <Text className="text-lg text-red-500 font-bold">This is the home screen</Text>
     </View>
   );
 }
