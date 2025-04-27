b35bf188317ad4f41e10826745e8abcf7ae92d8a
ashfaq_baloch5 <accoun7867@gmail.com>
2025-04-27 16:27:24 +0500

    Add some code

--- app/(roots)/(tabs)/index.tsx
+++ app/(roots)/(tabs)/index.tsx
@@ -1,10 +1,40 @@
+import Search from "@/components/Search";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center ">
-      <Text className=" font-bold text-lg my-10 text-red-500 font-rubik text-3xl">Welcome to the app!</Text>
-    </View>
+   <SafeAreaView className="bg-white h-full">
+     <View className="px-5">
+      <View className="flex flex-row items-center justify-between mt-5">
+        <View  className="flex flex-row items-center">
+          <Image
+          source={images.avatar}
+          className="size-12 rounded-full"
+          />
+          <View className="flex flex-col items-start ml-2 justify-center">
+            <Text className="text-xs font-rubik text-black-100">
+              Good Morning 
+            </Text>
+            <Text className="text-base font-rubik-medium text-black-300">
+              Ashfaq Haider
+            </Text>
+
+          </View>
+
+        </View>
+        <Image
+        source={icons.bell}
+        className="size-6"
+        />
+
+      </View>
+   
+     </View>
+     <Search />
+   </SafeAreaView>
   );
 }
