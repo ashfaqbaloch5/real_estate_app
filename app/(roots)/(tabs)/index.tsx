d1a44a19cfd1ffa42168916e264e1c4d9953b5e4
ashfaq_baloch5 <accoun7867@gmail.com>
2025-04-27 19:38:43 +0500

    Add some code

--- app/(roots)/(tabs)/index.tsx
+++ app/(roots)/(tabs)/index.tsx
@@ -3,18 +3,28 @@ import Filters from "@/components/Filters";
 import Search from "@/components/Search";
 import icons from "@/constants/icons";
 import images from "@/constants/images";
+import { useGlobalContext } from "@/lib/global-provider";
 import { Link } from "expo-router";
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
+  const {user}=useGlobalContext();
   return (
    <SafeAreaView className="bg-white h-full">
-     <View className="px-5">
+    <FlatList data={[1,2,3,4,5,6,7,8,9]}
+    renderItem={({ item }) => <Card />}
+    keyExtractor={(item) => item.toString()}
+    numColumns={2}
+    contentContainerClassName="pb--32"
+    columnWrapperClassName="flex gap-5 px-5"
+    showsVerticalScrollIndicator={false}
+    ListHeaderComponent={
+      <View className="px-5">
       <View className="flex flex-row items-center justify-between mt-5">
         <View  className="flex flex-row items-center">
           <Image
-          source={images.avatar}
+          source={{ uri: user?.avatar || images.avatar }}
           className="size-12 rounded-full"
           />
           <View className="flex flex-col items-start ml-2 justify-center">
@@ -47,11 +57,14 @@ export default function Index() {
         </TouchableOpacity>
 
       </View>
-        <View className="flex flex-row gap-5 mt-5">
-          <FeaturedCard/>
-          <FeaturedCard/>
-        </View>
-
+      <FlatList data={[1,2,3,4]}
+        renderItem={({ item }) => <FeaturedCard />}
+        keyExtractor={(item) => item.toString()}
+        horizontal
+        bounces={false}
+        showsHorizontalScrollIndicator={false}
+        contentContainerClassName="flex gap-5 mt-3"
+      />
       </View>
 
       <View className="flex flex-row items-center justify-between"> 
@@ -66,12 +79,11 @@ export default function Index() {
 
       </View>
       <Filters/>
-       <View className="flex flex-row gap-5 mt-5">
-         <Card />
-         <Card />
-
-       </View>
+      
      </View>
+    }
+    />
+     
     
      
    </SafeAreaView>
